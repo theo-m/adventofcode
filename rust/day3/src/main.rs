97cf385bacbb68d3b13db60c01b12b407e34b647
Théo Matussière <theo@matussie.re>
2021-12-03 16:24:21 +0100

    rework
    so many `.clone()`...

--- rust/day3/src/main.rs
+++ rust/day3/src/main.rs
@@ -50,57 +50,30 @@ fn main() {
 
     println!("part1: {},{} => {}", gamma, epsilon, gamma * epsilon); // 1025636
 
-    /// row major table
-    fn oxy(numbers: Vec<Vec<u8>>) -> u32 {
+    fn counter(numbers: Vec<Vec<u8>>, is_oxy: bool) -> u32 {
         let mut idx = 0;
         let mut winners: Vec<Vec<u8>> = numbers.clone();
-        fn step_i(idx: usize, numbers: Vec<Vec<u8>>) -> Vec<Vec<u8>> {
-            let tr = transpose(numbers.clone());
+
+        while winners.len() != 1 {
+            let tr = transpose(winners.clone());
             let curr = tr[idx].clone();
             let (zeros, ones): (Vec<u8>, Vec<u8>) = curr
                 .clone()
                 .iter()
                 .cloned()
                 .partition(|b| b.clone() == 0);
-            let most_common: u8 = if zeros.len() > ones.len() { 0 } else { 1 };
+            let mut most_common: u8 = if zeros.len() > ones.len() { 0 } else { 1 };
+            if !is_oxy { most_common = 1 - most_common }
+
             let winners_ids: Vec<usize> = curr.clone().iter().cloned().enumerate().filter(|(_, i)| i.clone() == most_common).map(|(i, _)| i).collect();
 
-            numbers.clone().iter().enumerate().filter(|(i, _)| winners_ids.contains(i)).map(|(_, v)| v.clone()).collect()
-        }
-
-        while winners.len() != 1 {
-            winners = step_i(idx, winners);
+            winners = winners.clone().iter().enumerate().filter(|(i, _)| winners_ids.contains(i)).map(|(_, v)| v.clone()).collect();
             idx += 1;
         }
 
         to_u32(winners.first().unwrap().clone())
     }
 
-    fn co2(numbers: Vec<Vec<u8>>) -> u32 {
-        let mut idx = 0;
-        let mut winners: Vec<Vec<u8>> = numbers.clone();
-        fn step_i(idx: usize, numbers: Vec<Vec<u8>>) -> Vec<Vec<u8>> {
-            let tr = transpose(numbers.clone());
-            let curr = tr[idx].clone();
-            let (zeros, ones): (Vec<u8>, Vec<u8>) = curr
-                .clone()
-                .iter()
-                .cloned()
-                .partition(|b| b.clone() == 0);
-            let most_common: u8 = if zeros.len() > ones.len() { 1 } else { 0 };
-            let winners_ids: Vec<usize> = curr.clone().iter().cloned().enumerate().filter(|(_, i)| i.clone() == most_common).map(|(i, _)| i).collect();
-
-            numbers.clone().iter().enumerate().filter(|(i, _)| winners_ids.contains(i)).map(|(_, v)| v.clone()).collect()
-        }
-
-        while winners.len() != 1 {
-            winners = step_i(idx, winners);
-            idx += 1;
-        }
-
-        to_u32(winners.first().unwrap().clone())
-    }
-
-    let (ox, co) = (oxy(numbers.clone()), co2(numbers.clone()));
-    println!("part2: {},{} => {}", ox, co, ox * co);
+    let (oxy, co2) = (counter(numbers.clone(), true), counter(numbers.clone(), false));
+    println!("part2: {},{} => {}", oxy, co2, oxy * co2); // 793873
 }
