ec83c0027681415dd3a719be0062ac357d51566f
Théo Matussière <theo@matussie.re>
2021-12-08 11:13:05 +0100

    dirty working day6p2

--- rust/day6/src/main.rs
+++ rust/day6/src/main.rs
@@ -1,4 +1,4 @@
-fn main() {
+fn main1() {
     let mut fishies = include_str!("../../../inputs/day6.txt")
         .split_once("\n").unwrap().0
         .split(",").map(|ns| ns.parse().unwrap())
@@ -16,4 +16,34 @@ fn main() {
         println!("day {}", _d);
     });
     println!("{:?}", fishies.len())
+}
+
+fn main() {
+    let fishies = include_str!("../../../inputs/day6.txt")
+        .split_once("\n").unwrap().0
+        .split(",").map(|ns| ns.parse().unwrap())
+        .collect::<Vec<u32>>();
+
+    let init_map = fishies.iter().fold(vec![0u64; 9], |mut acc, v| {
+        acc[*v as usize] += 1;
+        acc
+    });
+    println!("{:?}", init_map);
+    let cnt = (0..256).fold(init_map, |mut acc, _| {
+        println!("{:?}", acc);
+        let day0 = acc[0];
+        acc.clone().iter().enumerate().rev().for_each(|(day, num)| {
+            println!("{}:{}", day, num);
+            if day == 0 {
+                acc[8] = day0;
+                acc[6] += day0;
+            } else {
+                acc[day - 1] = *num;
+            }
+        });
+        println!("{:?}", acc);
+        acc
+    })
+        .iter().sum::<u64>();
+    println!("{:?}", cnt);
 }
