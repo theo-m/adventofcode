41cf072881fad67d13fad19200ad58bf9b44a3bc
Théo Matussière <theo@matussie.re>
2021-12-09 22:59:49 +0100

    working p2, v slow

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,19 +1,24 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, VecDeque};
+use std::hash::Hash;
 
-const CHARS: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890πåß∂ƒ∆Ω≈ç√∫";
-
-fn neighbors(w: usize, h: usize, i: usize, j: usize) -> Vec<(usize, usize)> {
+fn neighbors(m: &Vec<Vec<u8>>, i: usize, j: usize) -> Vec<((usize, usize), u8)> {
+    let (w, h) = (m.len(), m[0].len());
     [
         Some((i, j)),
         if i == 0 { None } else { Some((i - 1, j)) },
         if i == w - 1 { None } else { Some((i + 1, j)) },
         if j == 0 { None } else { Some((i, j - 1)) },
         if j == h - 1 { None } else { Some((i, j + 1)) },
-    ].iter().filter_map(|x| *x).collect()
+    ].iter()
+        .filter_map(|x| *x)
+        .map(|pos| (pos, m[pos.0][pos.1]))
+        .filter(|(_, v)| *v != 9)
+        .collect()
 }
 
+
 fn input() -> Vec<Vec<u8>> {
-    include_str!("../../inputs/day9.test2.txt")
+    include_str!("../../inputs/day9.txt")
         .lines()
         .map(|line|
             line.trim().split("")
@@ -48,47 +53,38 @@ fn part2() {
     let m = input();
     let (w, h) = (m.len(), m[0].len());
 
-    let mut ufind = HashMap::new();
+    let mut basins = vec![];
     m.iter()
         .flatten()
         .enumerate()
         .filter_map(|(i, d)| if *d == 9 { None } else { Some((i / h, i % h)) })
-        .for_each(|(x, y)| {
-            let reprs = neighbors(w, h, x, y).iter()
-                .filter_map(|pos| ufind.clone().get(pos).and_then(|n: &usize| Some((*pos, n.clone()))))
-                .collect::<Vec<((usize, usize), usize)>>();
+        .for_each(|pos| {
+            if basins.iter().flatten().collect::<HashSet<_>>().contains(&pos) { return; }
 
-            if reprs.is_empty() {
-                ufind.insert((x, y), x * h + y);
-            } else {
-                let (_, repr) = reprs[0];
-                reprs.iter().for_each(|(key, _)| { *ufind.get_mut(key).unwrap() = repr; });
-                ufind.insert((x, y), repr);
+            let mut n = HashSet::new();
+            n.insert(pos);
+
+            let mut q = VecDeque::from_iter(neighbors(&m, pos.0, pos.1));
+            while !q.is_empty() {
+                let ((x, y), _) = q.pop_front().unwrap();
+                neighbors(&m, x, y).iter().for_each(|it| {
+                    if !n.contains(&it.0) {
+                        q.push_back(*it);
+                        n.insert(it.0);
+                    }
+                });
             }
+            basins.push(n.clone());
         });
 
-    let basins_grouped = ufind.iter()
-        .fold(HashMap::<usize, Vec<(usize, usize)>>::new(), |mut acc, (pos, repr)| {
-            acc.entry(*repr).or_insert(vec![]).push(*pos);
-            acc
-        });
-    let mut basin_size = basins_grouped.iter()
-        .map(|(_repr, group)| group.len())
+    let mut basin_size = basins.iter()
+        .map(|set| set.len())
         .collect::<Vec<_>>();
     basin_size.sort();
     basin_size.reverse();
 
     println!("part2: num basins = {:?} ", basin_size.len());
     println!("part2: res = {:?} -> {}", basin_size.iter().take(3).collect::<Vec<_>>(), basin_size.iter().take(3).product::<usize>());
-    let cc = CHARS.chars().collect::<Vec<char>>();
-    println!("{}",
-             m.iter().enumerate()
-                 .map(|(i, seq)|
-                     seq.iter().enumerate()
-                         .map(|(j, h)|
-                             if *h == 9 { "■".to_string() } else { cc[ufind[&(i, j)] % cc.len()].to_string() })
-                         .fold(String::new(), |acc, v| acc + &v)
-                 ).fold(String::new(), |acc, v| acc + &v + "\n"));
 }
 
 fn main() {
